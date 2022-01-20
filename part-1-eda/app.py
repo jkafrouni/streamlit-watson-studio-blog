f304c27098f57a78df5b0ed724a7db67c37a5f7a
Jerome Kafrouni <jerome.kafrouni@ibm.com>
2022-01-20 14:17:30 -0500

    add part 1 eda

---
+++ part-1-eda/app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import cpd_helpers
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.header("Authenticate and pick a project and dataset")
+apikey = st.text_input("Your IBM Cloud API key", type='password', help='Find your API key [here](https://cloud.ibm.com/iam/apikeys)')
+
+auth_ok, headers, error_msg = cpd_helpers.authenticate(apikey)
+
+if not auth_ok:
+    st.error("You could not be authenticated. More details below.")
+    with st.expander("Expand to see the error message"):
+        st.write(error_msg)
+else:
+    st.success("You are successfully authenticated! Pick a project below.")
+    projects, error_msg = cpd_helpers.list_projects(headers)
+    _, project_id = st.selectbox("Pick a Watson Studio Project", projects, format_func=lambda x: f"{x[0]} (id: {x[1]}")
+
+if auth_ok:
+    datasets, error_msg = cpd_helpers.list_datasets(headers, project_id)
+    _, dataset_id = st.selectbox("Pick a Dataset to analyze", datasets, format_func=lambda x: f"{x[0]} (id: {x[1]}")
+    df, error_msg = cpd_helpers.load_dataset(headers, project_id, dataset_id)
+else:
+    st.write("Please authenticate first.")
+
+st.header("Dataset preview")
+if auth_ok:
+    n_rows = st.number_input("Number of rows", min_value=0, max_value=len(df), value=5, help='How many rows to sample and display.')
+    sample_strategy = st.radio("How to pick rows", ["Random Sample", "Head"])
+    if sample_strategy == "Random Sample":
+        st.write(df.sample(int(n_rows)))
+    else:
+        st.write(df.head(int(n_rows)))
+else:
+    st.write("Please authenticate first.")
+
+st.header("Visualizations")
+label = st.selectbox("Label column", list(df.columns))
+features = [c for c in df.columns if c != label]
+x_feature = st.selectbox("Feature (X axis)", features)
+
+st.subheader("Univariate distributions per class")
+st.plotly_chart(px.histogram(df, x=x_feature, color=label, marginal='box'))
+
+st.subheader("Average default rate per feature bin")
+st.markdown("This plot shows on the x axis a selected feature binned by quantile, \
+    and on the y axis the average rate of the label's positive class in each bin. \
+    The size of the bins can be selected with the slider below.")
+
+q_length = st.slider("Quantile size", min_value=0.01, max_value=0.5, step=0.01, value=0.05)
+bins = pd.qcut(df[x_feature], np.arange(0, 1.01, q_length), duplicates='drop')
+rate_per_bin = df.groupby(bins)[label].value_counts(normalize=True)
+fig, ax = plt.subplots()
+rate_per_bin.unstack().plot(kind='bar', stacked=True, ax=ax, rot=45)
+# adjust tick alignments, see https://stackoverflow.com/questions/35262475/controlling-tick-labels-alignment-in-pandas-boxplot-within-subplots
+plt.sca(ax)
+plt.xticks(ha='right')
+ax.set_xlabel(f"Bins of feature {x_feature}")
+ax.set_ylabel(f"Average rate of label {label}")
+st.pyplot(fig)
---
+++ part-1-eda/cpd_helpers.py
@@ -0,0 +1,134 @@
+import requests
+import pandas as pd
+
+CPD_URL = "https://api.dataplatform.cloud.ibm.com"
+
+
+# TODO, cache
+def authenticate(apikey):
+    """Calls the authentication endpoint for Cloud Pak for Data as a Service,
+    and returns authentication headers if successful.
+    See https://cloud.ibm.com/apidocs/watson-data-api#creating-an-iam-bearer-token.
+
+    Args:
+        apikey (str): An IBM Cloud API key, obtained from https://cloud.ibm.com/iam/apikeys).
+    Returns:
+        success (bool): Whether authentication was successful
+        headers (dict): If success=True, a dictionary with valid authentication headers. Otherwise, None.
+        error_msg (str): The text response from the authentication request if the request failed.
+    """
+    auth_headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic Yng6Yng=',
+    }
+
+    data = {
+        'apikey': apikey,
+        'grant_type': 'urn:ibm:params:oauth:grant-type:apikey'
+    }
+
+    r = requests.post('https://iam.ng.bluemix.net/identity/token', headers=auth_headers, data=data)
+
+    if r.ok:
+        headers = {"Authorization": "Bearer " + r.json()['access_token'], "content-type": "application/json"}
+        return True, headers, ""
+    else:
+        return False, None, r.text
+
+
+def list_projects(headers):
+    """Calls the project list endpoint of Cloud Pak for Data as a Service,
+    and returns a list of projects if successful.
+    See https://cloud.ibm.com/apidocs/watson-data-api#projects-list.
+
+    Args:
+        headers (dict): Authentication headers obtained with authenticate().
+    Returns:
+        projects (list): A list of (project_name, project_id) tuples.
+        error_msg (str): The text response from the request if the request failed.
+    """
+    r = requests.get(f"{CPD_URL}/v2/projects", headers=headers)
+    if r.ok:
+        projects = r.json()['resources']
+        parsed_projects = [(x['entity']['name'], x['metadata']['guid']) for x in projects]
+        return parsed_projects, ""
+    else:
+        return list(), r.text
+
+
+def list_datasets(headers, project_id):
+    """Calls the search endpoint of Cloud Pak for Data as a Service,
+    and returns a list of data assets in a given project if successful.
+    See https://cloud.ibm.com/apidocs/watson-data-api#simplesearch.
+
+    Args:
+        headers (dict): Authentication headers obtained with authenticate().
+        project_id (str): The Watson Studio project id to search in.
+    Returns:
+        datasets (list): A list of (dataset_name, dataset_id) tuples.
+        error_msg (str): The text response from the request if the request failed.
+    """
+    search_doc = {
+        "query": {
+            "bool": {
+                "must": 
+                [
+                    {"match": {"metadata.artifact_type": "data_asset"}},
+                    {"match": {"entity.assets.project_id": project_id}}
+                ]
+            }
+        }
+    }
+    r = requests.post(f"{CPD_URL}/v3/search",
+                      headers=headers,
+                      json=search_doc)
+
+    if r.ok:
+        datasets = r.json()['rows']
+        parsed_datasets = [(x['metadata']['name'], x['artifact_id']) for x in datasets]
+        return parsed_datasets, ""
+    else:
+        return list(), r.text
+
+
+# TODO cache
+def load_dataset(headers, project_id, dataset_id):
+    """Loads into a memory a data asset stored in a Watson Studio project
+    on IBM Cloud Pak for Data as a Service.
+    Abstracts away three steps:
+    - Retrieving a details of a data asset including its attachment id
+    - Retrieving the attachment
+    - Extracting a signed url from the attachment and using it to load the data into Pandas
+
+    Args:
+        headers (dict): Authentication headers obtained with authenticate().
+        project_id (str): The Watson Studio project id to search in.
+        dataset_id (str): The dataset to load
+    Returns:
+        df (pd.DataFrame): The dataset loaded into a Pandas DataFrame, empty if any of the HTTP requests fails.
+        error_msg (str): If any of the HTTP requests fails, the text response from the first failing
+            request.
+    """
+    r = requests.get(f"{CPD_URL}/v2/data_assets/{dataset_id}",
+                     params={"project_id": project_id},
+                     headers=headers
+                    )
+    if r.ok:
+        dataset_details = r.json()
+        attachment_id = dataset_details['attachments'][0]['id']
+    else:
+        return pd.DataFrame(), r.text
+
+    r2 = requests.get(f"{CPD_URL}/v2/assets/{dataset_id}/attachments/{attachment_id}",
+                      params={"project_id": project_id},
+                      headers=headers
+                      )
+    if r2.ok:
+        attachment_details = r2.json()
+    else:
+        return pd.DataFrame(), r2.text
+
+    try:
+        return pd.read_csv(attachment_details['url']), ""
+    except Exception as e:
+        return pd.DataFrame(), str(e)
